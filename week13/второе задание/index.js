a51ec69095a2d7bec0d5e542e864450493eeeae9
Nazgul <nazgulchan@gmail.com>
2022-04-30 01:07:05 +0500

    исправлено

--- week13/второе задание/index.js
+++ week13/второе задание/index.js
@@ -1,23 +1,23 @@
 function add () {
     let a = prompt('Введите число');
     let b = prompt('Введите число');
-    alert(Number(a) + Number(b))
+    alert(Number(a) + Number(b));
 }
 
 function subtract () {
     let a = prompt('Введите число');
     let b = prompt('Введите число');
-    alert(Number(a) - Number(b))
+    alert(Number(a) - Number(b));
 }
 
 function divide () {
     let a = prompt('Введите число');
     let b = prompt('Введите число');
-    alert(Number(a) / Number(b))
+    alert(Number(a) / Number(b));
 }
 
 function multiply () {
     let a = prompt('Введите число');
     let b = prompt('Введите число');
-    alert(Number(a) * Number(b))
+    alert(Number(a) * Number(b));
 }
